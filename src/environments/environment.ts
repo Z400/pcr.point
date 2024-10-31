111c6a62f941e737ce1775b9bb381d8241bd20aa
Matheus Viana <matheusgoldship@gmail.com>
2024-10-31 11:58:27 -0300

    Correção da url do backend!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:8080/api"
-    //https://pcr-point-image.onrender.com/api
+    apiUrl: "https://pcr-point-image.onrender.com/api"
     //http://localhost:8080/api
   };
