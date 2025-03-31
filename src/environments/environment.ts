518d051b645f240acdae84de208ffe99f8ec74b7
Matheus Viana <matheusgoldship@gmail.com>
2025-03-31 12:56:22 -0300

    correção dos options na parte de extrato!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:8080/api"
+    apiUrl: "https://pcr-point-image.onrender.com/api"
     //https://pcr-point-image.onrender.com/api
     //http://localhost:8080/api
   };
